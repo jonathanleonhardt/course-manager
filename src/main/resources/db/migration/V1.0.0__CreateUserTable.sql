5f71a449bc19f90766b0a9d011880613fac72f41
Jonathan Leonhardt <jonathanleonhardt18@gmail.com>
2024-04-16 19:41:41 -0300

    feat(migration): create initial entities migration

---
+++ src/main/resources/db/migration/V1.0.0__CreateUserTable.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS public.alura_user CASCADE;
+
+CREATE TABLE public.alura_user (
+	created_at timestamp NULL,
+	username varchar(20) NULL,
+	email text NULL,
+	id varchar(255) NOT NULL,
+	"name" text NOT NULL,
+	"password" text NOT NULL,
+	roles varchar(255) NULL,
+	CONSTRAINT alura_user_email_key UNIQUE (email),
+	CONSTRAINT alura_user_pkey PRIMARY KEY (id),
+	CONSTRAINT alura_user_username_key UNIQUE (username)
+);
---
+++ src/main/resources/db/migration/V2.0.0__CreateCourse.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS public.course;
+
+CREATE TABLE public.course (
+	active bool NULL,
+	created_at timestamp NULL,
+	inactivated_at timestamp NULL,
+	code varchar(20) NOT NULL,
+	description varchar(20) NOT NULL,
+	course_id varchar(255) NOT NULL,
+	id varchar(255) NOT NULL,
+	instructor_id varchar(255) NOT NULL,
+	"name" text NOT NULL,
+	student_id varchar(255) NOT NULL,
+	CONSTRAINT course_course_id_key UNIQUE (course_id),
+	CONSTRAINT course_pkey PRIMARY KEY (id),
+	CONSTRAINT course_student_id_key UNIQUE (student_id),
+	CONSTRAINT fk1mmionn9uhfddo2y9iqr21cmp FOREIGN KEY (student_id) REFERENCES public.alura_user(id),
+	CONSTRAINT fk2wm4tlyupif5o4galwhxs6y53 FOREIGN KEY (instructor_id) REFERENCES public.alura_user(id),
+	CONSTRAINT fksih9ud7kv7hk71nhv2cksbn0r FOREIGN KEY (course_id) REFERENCES public.course(id)
+);
