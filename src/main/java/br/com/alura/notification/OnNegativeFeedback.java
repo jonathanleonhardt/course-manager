52e439353c73ec3edbb906d7c6ba212f54b7ef4b
Jonathan Leonhardt <jonathanleonhardt18@gmail.com>
2024-04-19 00:39:54 -0300

    feat(feedback/notification): receive and send negative feedbacks by email

---
+++ src/main/java/br/com/alura/notification/OnNegativeFeedback.java
@@ -0,0 +1,43 @@
+package br.com.alura.notification;
+
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.core.event.NotifyFeedbackEvent;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OnNegativeFeedback {
+
+	@ApplicationModuleListener
+	void execute( NotifyFeedbackEvent event ) {
+		String recipient = this.getRecipient( event );
+		String subject = this.getSubject( event );
+		String body = this.getBody( event );
+		EmailSender.send( recipient, subject, body );
+	}
+
+	private String getRecipient( NotifyFeedbackEvent event ) {
+		return "%s - %s [ %s ]".formatted( event.instructorId(), event.instructorName(),
+				event.instructorEmail() );
+	}
+
+	private String getSubject( NotifyFeedbackEvent event ) {
+		return ".:: Negative feedback - %s ::.".formatted( event.courseCode() );
+	}
+
+	private String getBody( NotifyFeedbackEvent event ) {
+		return """
+
+					New negative feedback of %l in %s
+
+					STUDENT: %s - %s
+
+					DESCRIPTION: %s
+
+				""".formatted( event.rating(), event.courseCode(), event.studentId(),
+				event.studentId(), event.description() );
+	}
+
+}
